57a7c2441c48882b5c8d9aad5c762f9ea04ca25e
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-04-26 19:41:37 +0000

    inversed the if else statement to count how many tries left or no

--- hangman.js
+++ hangman.js
@@ -37,51 +37,50 @@ var prompt = require("prompt");
 prompt.start(); // Start the prompt 
 
 function userGuess() {
-    if (wrongGuesses.length < 10) {
-        prompt.get({
-            properties: {
-                letter: {
-                    //Validate if the user input is one lowercase letter
-                        description: "Guess a letter",
-                        pattern: /[a-z]/,
-                        message: "Letter must be one lowercase letter",
-                        required: true,
-                        maxLength: 1
-                }
+    if (wrongGuesses.length > 10) {
+        console.log("It's over. You hanged the man. The word is " + word + ". Your guesses were " + wrongGuesses + ". ");
+        return;
+    }
+    
+    prompt.get({
+        properties: {
+            letter: {
+                //Validate if the user input is one lowercase letter
+                    description: "Guess a letter",
+                    pattern: /[a-z]/,
+                    message: "Letter must be one lowercase letter",
+                    required: true,
+                    maxLength: 1
             }
-        }, function(err, result) {
-                var guess = result.letter;
-                var index = arrOfLetters.indexOf(guess);
-                //check if the letter is in the word
-            //If the letter isn't, add it to wrongGuesses array and display the hangman and the array of wrongGuesses    
-                if (index === -1) {
-                    wrongGuesses.push(guess);
-                    var hanged = hangman.slice(0, wrongGuesses.length).join("");
-                    console.log("The guessed word doesn't have: " + wrongGuesses);
-                    console.log(hanged);
+        }
+    }, function(err, result) {
+            var guess = result.letter;
+            var index = arrOfLetters.indexOf(guess);
+            //check if the letter is in the word
+        //If the letter isn't, add it to wrongGuesses array and display the hangman and the array of wrongGuesses    
+            if (index === -1) {
+                wrongGuesses.push(guess);
+                var hanged = hangman.slice(0, wrongGuesses.length).join("");
+                console.log("The guessed word doesn't have: " + wrongGuesses);
+                console.log(hanged);
+                userGuess();
+            }
+        //Find the index or indices where is the letter and replace the underscore by the guessed letter
+            else {
+                while (index !== -1) {
+                    arrOfGuessing.splice(index, 1, guess);
+                    index = arrOfLetters.indexOf(guess, index + 1);
+                    guessedLetters++;
+                }
+                console.log(arrOfGuessing);
+                if (guessedLetters === word.length) {
+                    console.log("Congratulations! You found the word : " + word + "!");
+                }
+                else {
                     userGuess();
                 }
-            //Find the index or indices where is the letter and replace the underscore by the guessed letter
-                else {
-                    while (index !== -1) {
-                        arrOfGuessing.splice(index, 1, guess);
-                        index = arrOfLetters.indexOf(guess, index + 1);
-                        guessedLetters++;
-                    }
-                    console.log(arrOfGuessing);
-                    if (guessedLetters === word.length) {
-                        console.log("Congratulations! You found the word : " + word + "!");
-                    }
-                    else {
-                        userGuess();
-                    }
-                }
-        });
-
-    }
-    else {
-        console.log("It's over. You hanged the man. The word is " + word + ". Your guesses were " + wrongGuesses + ". ");
-    }
+            }
+    });
 }
 userGuess();
 
